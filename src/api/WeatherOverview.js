9e8093f6e8ef0ea9244516e7db37b9f8ab3e86d1
Christian Blom <christian.blom@chasacademy.se>
2019-03-09 08:46:56 +0100

    Added wind speed and humidity.

--- src/api/WeatherOverview.js
+++ src/api/WeatherOverview.js
@@ -3,18 +3,20 @@ import Skycons from 'react-skycons';
 import '../index.css';
 
 function WeatherOverview(props) {
-  const { weather } = props;
+  const { weather, isLoaded } = props;
 
-  if (props.isLoaded) {
+  if (isLoaded) {
     return (
       <section className="weatherBox">
-        <h2>{weather.currently.temperature.toFixed(1)}°</h2>
+        <h2>{weather.currently.temperature.toFixed(0)}°</h2>
         <Skycons
         icon={weather.currently.icon.toUpperCase().replace('-', '_')}
         style={{}}
         width="64px"
         height="64px"
         />
+        <p>{weather.currently.humidity * 100}% Humidity</p>
+        <p>Wind Speed: {weather.currently.windSpeed} {weather.flags.units === 'si' ? 'm/s' : 'mph'}</p>
         <p>{weather.currently.summary}</p>
       </section>
     )
